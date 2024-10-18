48bd27fb5cf3d59e37188d1790fb08b5d8057477
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-18 14:24:53 +0200

    complete palindrome

--- JS/palindroma.js
+++ JS/palindroma.js
@@ -1,3 +1,30 @@
 // ! Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const userChoice = prompt("Inserisci una parola");
+const reverseduserChoice = stringreverse(userChoice);
+const isuserChoicepalindrome = isPalindrome(userChoice);
+
+function stringreverse(string) {
+  const reversedstring = string.split("").reverse().join("");
+  return reversedstring;
+}
+
+function isPalindrome(string) {
+  const reversedstring = string.split("").reverse().join("");
+
+  if (reversedstring === string) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(userChoice);
+console.log(reverseduserChoice);
+console.log(
+  isuserChoicepalindrome
+    ? `${userChoice} è una parola palindroma!`
+    : `${userChoice} non è una parola palindroma!`
+);
